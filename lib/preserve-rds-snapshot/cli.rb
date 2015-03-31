37b6e2ad81fdcc94beb888787bef78053d07083d
ISOBE Kazuhiko <muramasa64@gmail.com>
2015-03-31 10:35:27 +0900

    list snapshots

--- lib/preserve-rds-snapshot/cli.rb
+++ lib/preserve-rds-snapshot/cli.rb
@@ -6,9 +6,21 @@ module PreserveRdsSnapshot
     include Thor::Aws
 
     desc :list, 'Show list of RDS Snapshots'
-
+    option :snapshot_type,
+      aliases: [:t],
+      type: :string,
+      desc: "snapshot type (manual or automated)"
     def list
-      p rds.client.describe_db_snapshots.to_a
+      begin
+        resp = rds.client.describe_db_snapshots(
+          snapshot_type: options[:snapshot_type]
+        )
+        resp.db_snapshots.each do |s|
+          puts "#{s.db_snapshot_identifier}\t#{s.snapshot_create_time}"
+        end
+      rescue ::Aws::Errors::ServiceError => e
+        $stderr.puts e
+      end
     end
   end
 end
