1716fcec44f82588f5b5fadf6502e9e086e276bb
ISOBE Kazuhiko <muramasa64@gmail.com>
2015-03-31 12:51:41 +0900

    preserve command that auto copy latest snapshots

--- lib/preserve-rds-snapshot/cli.rb
+++ lib/preserve-rds-snapshot/cli.rb
@@ -30,6 +30,28 @@ module PreserveRdsSnapshot
     end
 
     desc :preserve, 'copy automated snapshot to manual'
+    def preserve
+      begin
+        instances = db_instances(options[:instance])
+        instances.each do |i|
+          latest = latest_auto_snapshot(i.db_instance_identifier)
+          if latest
+            db_snapshot_identifier = latest.db_snapshot_identifier
+            resp = rds.client.copy_db_snapshot(
+              source_db_snapshot_identifier: db_snapshot_identifier,
+              target_db_snapshot_identifier: preserve_snapshot_name(db_snapshot_identifier),
+              tags: [key: 'type', value: 'preserve']
+            )
+            s = resp.db_snapshot
+            puts "#{s.db_snapshot_identifier}\t#{s.snapshot_create_time}"
+          end
+        end
+      rescue ::Aws::Errors::ServiceError => e
+        $stderr.puts e
+      end
+    end
+
+    desc :copy, 'copy  snapshot'
     option :source_db_snapshot_identifier,
       aliases: [:o],
       type: :string,
@@ -40,7 +62,7 @@ module PreserveRdsSnapshot
       type: :string,
       desc: 'target snapshot identifier',
       required: true
-    def preserve
+    def copy
       begin
         resp = rds.client.copy_db_snapshot(
           source_db_snapshot_identifier: options[:source_db_snapshot_identifier],
@@ -56,8 +78,11 @@ module PreserveRdsSnapshot
 
     desc :latest, 'show latest snapshot'
     def latest
-      s = latest_auto_snapshot(options[:instance])
-      puts "#{s.db_snapshot_identifier}\t#{s.snapshot_create_time}" if s
+      instances = db_instances(options[:instance])
+      instances.each do |instance|
+        s = latest_auto_snapshot(instance.db_instance_identifier)
+        puts "#{s.db_snapshot_identifier}\t#{s.snapshot_create_time}" if s
+      end
     end
 
     private
@@ -74,6 +99,23 @@ module PreserveRdsSnapshot
         $stderr.puts e
       end
       latest
+    end
+
+    def db_instances(db_instance_identifier = nil)
+      list = []
+      begin
+        if db_instance_identifier
+          list << rds.db_instance(db_instance_identifier)
+        else
+          list = rds.db_instances.to_a
+        end
+      rescue ::Aws::Errors::ServiceError => e
+        $stderr.puts e
+      end
+    end
+
+    def preserve_snapshot_name(db_snapshot_identifier)
+      'preserve-' + db_snapshot_identifier.gsub(/^rds:/, '')
     end
   end
 end
